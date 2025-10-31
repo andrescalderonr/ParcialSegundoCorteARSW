c5beafe6e0861ab0ff47d7bf3582220720ed6074
unknown <andres.calderon-r@labinfo.is.escuelaing.edu.co>
2025-10-31 07:57:44 -0500

    Rest controller implementado con requestmapping

--- src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
@@ -13,20 +13,9 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest()
+
 public class ApplicationServicesTests {
 
-    
-    RestaurantOrderServicesStub ros;
 
-    
-    @Test
-    public void contextLoads() throws OrderServicesException{
-        
-        
-        
-        
-    }
 
 }
